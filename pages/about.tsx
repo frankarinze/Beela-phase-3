db422a06f37f28a9edbea5cdd4f5b368c571f375
Roshni Patel <rpatel01@wesleyan.edu>
2022-05-27 13:38:26 -0500

    Working on about page

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
+const About: NextPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <Footer /> 
+        </div>
+    )
+}
+
+export default About; 
